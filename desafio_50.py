955aa8af66291ed98ffff98ec14765945b910d65
Talita <talita-sdreis@educar.rs.gov.br>
2022-08-23 20:17:47 -0300

    refactory

--- desafio_50.py
+++ desafio_50.py
@@ -2,6 +2,7 @@
 Desenvolva um programa que leia seis números inteiros
 e mostre a soma apenas daqueles que forem pares
 Se o valor digitado for ímpar, desconsidere-o.'''
+
 soma = 0
 num = input('Digite 6 numeros inteiros: ')
 seis_num = num.split(" ")
