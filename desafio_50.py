1cc780eb617a67f54ea0d33a88bb3f9dc5714ebd
Talita <talita-sdreis@educar.rs.gov.br>
2022-08-23 20:11:12 -0300

    desafio cocluido

--- desafio_50.py
+++ desafio_50.py
@@ -2,3 +2,12 @@
 Desenvolva um programa que leia seis números inteiros
 e mostre a soma apenas daqueles que forem pares
 Se o valor digitado for ímpar, desconsidere-o.'''
+soma = 0
+num = input('Digite 6 numeros inteiros: ')
+seis_num = num.split(" ")
+for n in seis_num:
+    n = int(n)
+    par =  n % 2 == 0
+    if par == True:
+        soma += n
+print(f'a soma dos numeros pares e igual a {soma}')
